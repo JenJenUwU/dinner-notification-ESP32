732882bf0a6cec9de4de3c73476ea55e64167488
JenJenUwU <75403961+JenJenUwU@users.noreply.github.com>
2023-10-26 21:41:02 -0400

    Add files via upload

---
+++ arduino_secrets.h
@@ -0,0 +1,5 @@
+#define SECRET_SSID "littleroll_toronto"
+#define SECRET_PASS "61723249286172324928"
+//Copy the webhook url here:
+#define SECRET_WEBHOOK "https://discord.com/api/webhooks/1165402285849976892/jJ0IdPAdnmhPH4e7NTTdTH9Qvog4gjwCjOMcN9STYbd6EjA4Wki9Xa_SincqqwwVFC3e"
+#define SECRET_TTS "true"
